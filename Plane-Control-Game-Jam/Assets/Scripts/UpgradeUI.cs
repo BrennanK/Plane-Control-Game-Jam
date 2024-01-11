71d54b07767f0d100a08c3d6bdfe0f3017cdb6fe
BrennanK <bksafari17@gmail.com>
2024-01-11 16:08:47 -0600

    Upgrade UI so far

--- Plane-Control-Game-Jam/Assets/Scripts/UpgradeUI.cs
+++ Plane-Control-Game-Jam/Assets/Scripts/UpgradeUI.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 public class UpgradeUI : MonoBehaviour
 {
-    //[SerializeField]
-    //private List<GameObject> buttons;
+    [SerializeField]
+    private List<GameObject> panel;
     // Start is called before the first frame update
 
     private void Start()
@@ -13,6 +14,19 @@ public class UpgradeUI : MonoBehaviour
         //GameObject newButton = Instantiate(buttons[0],buttons[1].transform);
         //newButton.GetComponent<RectTransform>().anchoredPosition = buttons[1].GetComponent<RectTransform>().anchoredPosition;
 
+        panel[0].transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = "HP +" + Upgrade_Manager.instance.getHealthUpgrade();
+        panel[1].transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = "Speed +" + Upgrade_Manager.instance.getSpeedUpgrade();
+        panel[2].transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = "Damage +" + Upgrade_Manager.instance.getWeaponDamgeUpgrade();
+
+       // Upgrade_Manager.instance.chooseWeapon();
+
+        //panel[3].transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = Upgrade_Manager.instance.getWeaponName();
+
+    }
+
+    private void OnEnable()
+    {
+        panel[3].transform.GetChild(0).gameObject.GetComponent<TMP_Text>().text = Upgrade_Manager.instance.getWeaponName();
     }
     public void HealthUpgrade()
     {
